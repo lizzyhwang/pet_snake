fa733bd2319c442f8de39c0d7ecec9c5769f2497
Lizzy Hwang <hwang.eli@northeastern.edu>
2024-03-18 14:42:56 -0400

    bkg music; q to quit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 from snake import Snake
+from pygame import mixer
 
 # pygame setup
 pygame.init()
+
 mWindowSize = 720
 screen = pygame.display.set_mode((mWindowSize, mWindowSize))
 clock = pygame.time.Clock()
@@ -13,12 +15,20 @@ mSnake = Snake(mTileSize, mWindowSize)
 headColor = (255,62,184)
 bodyColor = (0,0,0)
 
+mixer.init()
+mixer.music.load("flight_of_bumblebees_8bit.wav") # .wav required, bugs with .mp3
+mixer.music.set_volume(1.0)
+mixer.music.play(-1) # -1 makes the music loop indefinitely
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                running = False
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
@@ -35,4 +45,5 @@ while running:
 
     clock.tick(10)  # limits FPS to 2
 
+mixer.music.stop()
 pygame.quit()
